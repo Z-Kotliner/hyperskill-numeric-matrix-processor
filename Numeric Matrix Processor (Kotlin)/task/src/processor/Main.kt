91176f4a11d190130f027a568b8f1b4033c3cd64
Omar Osman <zkotliner@gmail.com>
2025-06-24 14:45:25 +0900

    Stage 1/6: Matrix Addition

--- Numeric Matrix Processor (Kotlin)/task/src/processor/Main.kt
+++ Numeric Matrix Processor (Kotlin)/task/src/processor/Main.kt
@@ -1,5 +1,29 @@
 package processor
 
 fun main() {
-    println("Hello World!")
+    val (n, m) = readln().split(" ").map { it.toInt() }
+    val matrixA = Array(n) { readln().split(" ").map { it.toInt() }.toIntArray() }
+
+
+    val (n1, m1) = readln().split(" ").map { it.toInt() }
+    val matrixB = Array(n1) { readln().split(" ").map { it.toInt() }.toIntArray() }
+
+    if (n1 != n || m1 != m) {
+        println("ERROR")
+        return
+    }
+
+    val sumMatrix = matrixSum(matrixA, matrixB)
+    printMatrix(sumMatrix)
+}
+
+fun matrixSum(matrixA: Array<IntArray>, matrixB: Array<IntArray>): Array<IntArray> =
+    matrixA.zip(matrixB).map { pair -> pair.first.zip(pair.second).map { it.first + it.second }.toIntArray() }
+        .toTypedArray()
+
+fun printMatrix(matrix: Array<IntArray>) {
+    println()
+    matrix.forEach {
+        println(it.joinToString(" "))
+    }
 }
