e6f08d490b37ef1da689646938077133046f2f9b
Omar Osman <zkotliner@gmail.com>
2025-06-25 17:35:52 +0900

    Stage 2/6: Matrix Multiplication

--- Numeric Matrix Processor (Kotlin)/task/src/processor/Main.kt
+++ Numeric Matrix Processor (Kotlin)/task/src/processor/Main.kt
@@ -4,22 +4,17 @@ fun main() {
     val (n, m) = readln().split(" ").map { it.toInt() }
     val matrixA = Array(n) { readln().split(" ").map { it.toInt() }.toIntArray() }
 
+    val c = readln().toInt()
 
-    val (n1, m1) = readln().split(" ").map { it.toInt() }
-    val matrixB = Array(n1) { readln().split(" ").map { it.toInt() }.toIntArray() }
-
-    if (n1 != n || m1 != m) {
-        println("ERROR")
-        return
-    }
-
-    val sumMatrix = matrixSum(matrixA, matrixB)
-    printMatrix(sumMatrix)
+    printMatrix(matrixScalarProduct(matrixA, c))
 }
 
 fun matrixSum(matrixA: Array<IntArray>, matrixB: Array<IntArray>): Array<IntArray> =
     matrixA.zip(matrixB).map { pair -> pair.first.zip(pair.second).map { it.first + it.second }.toIntArray() }
         .toTypedArray()
+
+fun matrixScalarProduct(matrix: Array<IntArray>, c: Int): Array<IntArray> =
+    matrix.map { row -> row.map { it * c }.toIntArray() }.toTypedArray()
 
 fun printMatrix(matrix: Array<IntArray>) {
     println()
